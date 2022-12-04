ec157dec5c93e77a4a727cc90a265a9db33fb800
yajme <spartan.emjayrongavilla@gmail.com>
2022-12-04 12:40:50 +0800

    Basic Functions

--- Form1.cs
+++ Form1.cs
@@ -17,32 +17,15 @@ namespace FinalProjectOOP_Calculator
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            txtDisplay.Text += button.Text;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button21_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void operation_click(object sender, EventArgs e)
         {
 
         }
